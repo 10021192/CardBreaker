909f429a8465bce6f0b4a4974d906f34de7d3667
Afonso Salgueiro <afonsoduartesalgueiro@gmail.com>
2025-06-19 00:36:39 +0100

    modified basic route to redirect to new url

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(session({
 
 // Basic routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/game/index.html'));
+    res.redirect('/game/');
 });
 
 // Test routes
