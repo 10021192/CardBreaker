ebbe29450463e842f40b3b186d1e26d8fc53b642
Afonso Salgueiro <afonsoduartesalgueiro@gmail.com>
2025-06-19 19:56:35 +0100

    revert last commit

--- public/game/src/scenes/Lobby.js
+++ public/game/src/scenes/Lobby.js
@@ -251,9 +251,7 @@ class Lobby extends Phaser.Scene {
 		try {
 			const response = await fetch('https://cardbreaker.onrender.com/api/games/active', {
 				credentials: 'include'
-			})
-			.then(r => r.json())
-  			.then(console.log);
+			});
 			const data = await response.json();
 
 			if (data.games) {
